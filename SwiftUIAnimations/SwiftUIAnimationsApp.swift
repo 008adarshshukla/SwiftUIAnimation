23c182bd5790d58c2731e73cefc932f59d7fd9da
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-04-01 22:21:28 +0530

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Adarsh Shukla on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
