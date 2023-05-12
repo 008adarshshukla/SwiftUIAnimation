49169868363db70db9de276c4d1fc4f331e550bc
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-05-12 19:27:37 +0530

    Initiated book loading animation

---
+++ SwiftUIAnimations/BookLoadingAnimation/BookHolderView.swift
@@ -0,0 +1,20 @@
+//
+//  BookHolderView.swift
+//  SwiftUIAnimations
+//
+//  Created by Adarsh Shukla on 12/05/23.
+//
+
+import SwiftUI
+
+struct BookHolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookHolderView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookHolderView()
+    }
+}
---
+++ SwiftUIAnimations/BookLoadingAnimation/BookLoaderView.swift
@@ -0,0 +1,20 @@
+//
+//  BookLoaderView.swift
+//  SwiftUIAnimations
+//
+//  Created by Adarsh Shukla on 12/05/23.
+//
+
+import SwiftUI
+
+struct BookLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookLoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookLoaderView()
+    }
+}
