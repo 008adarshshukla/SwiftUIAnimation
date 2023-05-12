3dcd5e7fef49fdfbee16f807f8df51806bd00597
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-05-12 17:56:51 +0530

    Done Breathe app animation

--- SwiftUIAnimations/BreathingAppAnimation.swift
+++ SwiftUIAnimations/BreathingAppAnimation.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct BreathingAppAnimation: View {
+    
+    @State private var performAnimation: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            Group {
+                VStack(spacing: 5) {
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint: .bottom)).opacity(0.5)
+                        .frame(width: 150, height: 150)
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .bottom, endPoint: .top)).opacity(0.5)
+                        .frame(width: 150, height: 150)
+                }
+                
+                
+                VStack(spacing: 5) {
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint: .bottom)).opacity(0.5)
+                        .frame(width: 150, height: 150)
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .bottom, endPoint: .top)).opacity(0.5)
+                        .frame(width: 150, height: 150)
+                }
+                .rotationEffect(Angle(degrees: 60))
+                
+                VStack(spacing: 5) {
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint: .bottom)).opacity(0.5)
+                        .frame(width: 150, height: 150)
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .bottom, endPoint: .top)).opacity(0.5)
+                        .frame(width: 150, height: 150)
+                }
+                .rotationEffect(Angle(degrees: 120))
+            }
+            .rotationEffect(performAnimation ? .degrees(90) : .degrees(0))
+            .scaleEffect(performAnimation ? 1 : 1/8)
+            .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true), value: performAnimation)
+        }
+        .onAppear {
+            performAnimation = true
+        }
     }
 }
 
