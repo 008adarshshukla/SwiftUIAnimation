021bac1e17ec1cbf291415708756249c2f12d39e
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-05-14 09:25:48 +0530

    Started the animated Tab Bar

---
+++ SwiftUIAnimations/AnimatedTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  AnimatedTabBar.swift
+//  SwiftUIAnimations
+//
+//  Created by Adarsh Shukla on 14/05/23.
+//
+
+import SwiftUI
+
+struct AnimatedTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimatedTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatedTabBar()
+    }
+}
